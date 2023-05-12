d39d431677906db22da2ead0777dbdb980621fe4
Brad Webber <bradweb@amazon.com>
2023-05-12 15:38:28 -0500

    Specify content root.

--- app/Bookstore.Web/Program.cs
+++ app/Bookstore.Web/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using Bookstore.Web.Startup;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseContentRoot(AppContext.BaseDirectory);
 
 builder.ConfigureConfiguration();
 
